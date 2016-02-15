06b52cc395a219a9aaf575a06825204b24ca7e49
Federico-PC <Federico-PC@Federico.lan>
2016-02-15 18:38:42 +0100

    Ho caricato la nuova classe user interface che carica un immagine

--- client/src/client/MainClass.java
+++ client/src/client/MainClass.java
@@ -1,10 +1,19 @@
 package client;
 
-public class MainClass {
+import user_interface.UserInterface;
+ 
+public class MainClass   {
+	
+	   public static void main(String[] args){
+		   UserInterface  user_interface = new UserInterface();  
+		   user_interface.setVisible(true);
+	   }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	  
 
+	
 	}
 
-}
+
+
+
---
+++ client/src/user_interface/UserInterface.java
@@ -0,0 +1,55 @@
+package user_interface;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.*;
+
+public class  UserInterface extends Frame {
+
+	//Costruttore dela classe che va a chiamare il metodo PrepareGui()
+	 public UserInterface(){  
+	      super("Munchkin");
+	      prepareGUI();
+	   }
+      
+	 public void paint(Graphics g) {
+		    Image img = createImage();
+		    g.drawImage(img, 20,20,this);
+		  }
+	 
+	//Metodo che gestisce la finestra settando altezza e larghezza
+	 
+	  private void prepareGUI(){  
+	      setSize(400,400);
+	      addWindowListener(new WindowAdapter() {
+	         public void windowClosing(WindowEvent windowEvent){
+	            System.exit(0);
+	         }        
+	      }); 
+	   }    
+	  
+	/*metodo che va a caricare l'immagine, mediante la libreria ImageIO, 
+		restituendo poi al metodo paint() l'immagine caricata. Inoltre dato
+		che tale caricamento potrebbe non restituire niente e quindi conviene
+		gestire tale problema con un'eccezione.
+	 */
+	   private Image createImage(){     
+		   BufferedImage img = null;
+		   try {
+			    img = ImageIO.read(new File("/Resources/Images/homeimage.png"));
+			} catch (IOException e) {
+			}
+		    
+		    
+		    return img;
+		  }
+	   
+     
+        
+}
