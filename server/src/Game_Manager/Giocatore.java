aeb82f154604a10bc52a19bebfe16c58705793d9
Simone <Simone@192.168.1.111>
2016-02-16 20:36:02 +0100

    creato un package per il gioco vero e proprio e creazione della classe giocatore

---
+++ server/src/Game_Manager/Giocatore.java
@@ -0,0 +1,22 @@
+package Game_Manager;
+
+public class Giocatore {
+	
+	protected String username;
+	protected int level;
+	
+	public Giocatore() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void dead()
+	{
+		
+	}
+	
+	public void levele_up()
+	{
+		
+	}
+
+}
