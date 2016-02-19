ddcb2cd9e8559f4ff018ee6820ca170dfd707d49
Matteo Cappelletti <matcap94@gmail.com>
2016-02-19 12:50:05 +0100

    Added image loading in MunchkinClient

--- client/src/client/MunchkinClient.java
+++ client/src/client/MunchkinClient.java
@@ -1,43 +1,58 @@
 package client;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import javax.swing.JPanel;
 
+import image.XmlImageLoader;
+import javafx.util.Pair;
 import user_interface.GameUI;
 import user_interface.Game_Window;
 import user_interface.MenuUI;
 import user_interface.LobbyUI;
 import user_interface.PauseUI;
- 
-public class MunchkinClient   {
+
+public class MunchkinClient {
+
 	private static JPanel[] panels = new JPanel[4];
-	
-	
-	public MunchkinClient(){
-		panels[0]=new GameUI();
-		panels[1]=new LobbyUI();
-		panels[2]=new MenuUI();
-		panels[3]=new PauseUI();
-		
+	private static HashMap<String, BufferedImage> images;
+
+	public MunchkinClient() {
+		panels[0] = new GameUI();
+		panels[1] = new LobbyUI();
+		panels[2] = new MenuUI();
+		panels[3] = new PauseUI();
+
 	}
-	
+
 	public static JPanel getPanel(int i) {
 		return panels[i];
 	}
-	
-	   public static void main(String[] args){
-		   
-			MunchkinClient client=new MunchkinClient();
-	  
+
+	public static void main(String[] args) {
+
+		MunchkinClient client = new MunchkinClient();
+
 		Game_Window window = new Game_Window();
 		window.setVisible(true);
-		   
-	   }
 
-	  
-
-	
+		XmlImageLoader loader = null;
+		try{
+			loader = new XmlImageLoader(new File("Resources/Images/images.xml"));
+			loader.load();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		images = new HashMap<String,BufferedImage>();
+		for(Pair<String, BufferedImage> pair : loader.getImages()){
+			images.put(pair.getKey(), pair.getValue());
+		}
+		
 	}
 
-
-
-
+}
