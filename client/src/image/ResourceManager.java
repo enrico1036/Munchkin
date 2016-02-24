8e2a7d07a03942e414b486015c2cb1d908cbf110
Matteo Cappelletti <matcap94@gmail.com>
2016-02-24 03:44:49 +0100

    Separated resource loading into static ResourceManager

---
+++ client/src/image/ResourceManager.java
@@ -0,0 +1,32 @@
+package image;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.HashMap;
+
+import javafx.util.Pair;
+
+public class ResourceManager {
+	private static HashMap<String, BufferedImage> images;
+
+	private static void loadAll() {
+		// Load images from resource folder
+		XmlImageLoader loader = null;
+		try {
+			loader = new XmlImageLoader(new File("Resources/Images/images.xml"));
+			loader.load();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		images = new HashMap<String, BufferedImage>();
+		for (Pair<String, BufferedImage> pair : loader.getImages()) {
+			images.put(pair.getKey(), pair.getValue());
+		}
+	}
+
+	public static BufferedImage getImage(String Name) {
+		return images.get(Name);
+	}
+
+}
