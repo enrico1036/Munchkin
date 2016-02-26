6a7f2fad86e2415da5bd8cbcfdb80303f86d9ef9
Enrico Gambini <enrico1036@gmail.com>
2016-02-26 16:56:51 +0100

    Removed MainClass.java from repo

--- server/src/server/MainClass.java
+++ server/src/server/MainClass.java
@@ -3,15 +3,11 @@ package server;
 import java.io.IOException;
 
 import game.GameManager;
-import network.ConnectionHandler;
 
 public class MainClass {
 
 	public static void main(String[] args) throws IOException {
-//		ConnectionHandler handler = new ConnectionHandler(34569, 3);
-//		handler.setAcceptTimeout(60000);
-//		handler.setConnectionTimeout(10000);
-//		handler.run();
+
 		GameManager.StartGame();
 		
 	}
