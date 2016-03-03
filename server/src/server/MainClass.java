bc30301c00a3fb0b23c4b9aac570d1bb98d96c80
Federico Simonetti <federicosimonetti94@gmail.com>
2016-03-03 18:27:43 +0100

    Added the XmlCardLoader in the Server main class

--- server/src/server/MainClass.java
+++ server/src/server/MainClass.java
@@ -1,8 +1,12 @@
 package server;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 import game.GameManager;
+
 import javafx.util.Pair;
 import network.ConnectionListener;
 import network.ConnectionPool;
@@ -12,6 +16,8 @@ import utils.Debug;
 
 public class MainClass {
 
+	private static HashMap<String, String> cards;
+	
 	public static void main(String[] args) throws IOException {
 		MessageQueue queue = new MessageQueue();
 		ConnectionPool pool = new ConnectionPool(6, queue);
@@ -41,7 +47,21 @@ public class MainClass {
 				e.printStackTrace();
 			}
 		}
+
 		
+			XmlCardLoader loader = null;
+			try{
+				loader = new XmlCardLoader(new File("Resources/cards.xml"));
+				loader.load();
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+			
+			cards = new HashMap<String,String>();
+			for(Pair<String, String> pair : loader.getCards()){
+				cards.put(pair.getKey(), pair.getValue());
+			}
 		
 		GameManager.startGame();
 		
