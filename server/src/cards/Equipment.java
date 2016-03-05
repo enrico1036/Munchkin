71f0085015b0289f00774ad39aaf0fae7c2566ad
Federico Simonetti <federicosimonetti94@gmail.com>
2016-03-05 01:52:48 +0100

    Completed the XmlCardLoader on the server. Needs to fix some code lines

--- server/src/cards/Equipment.java
+++ server/src/cards/Equipment.java
@@ -5,8 +5,11 @@ public class Equipment extends Card {
 	private int value;
 	private EquipSlot slot;	
 
-	public Equipment(String title, EquipSlot slot) {
+	public Equipment(String title){
 		this.title = title;
+	}
+	
+	public void createEquipment(EquipSlot slot,int bonus,int value) {
 		this.type = CardType.Treasure;
 		this.slot = slot;
 		this.category = Category.Equipment;
@@ -21,5 +24,33 @@ public class Equipment extends Card {
 		// TODO Auto-generated method stub
 
 	}
+	
+	public EquipSlot getSlot(String slot){
+		
+		EquipSlot eqSlot=null;
+		
+		switch(slot){
+		
+			case"head":
+				eqSlot=EquipSlot.head;
+				break;
+			case"body":
+				eqSlot=EquipSlot.body;
+				break;
+			case"hand1":
+				eqSlot=EquipSlot.hand1;
+				break;
+			case"hand2":
+				eqSlot=EquipSlot.hand2;
+				break;
+			case"feet":
+				eqSlot=EquipSlot.feet;
+				break;
+			
+		}
+		return eqSlot;
+	}
+	
+
 
 }
--- server/src/server/XmlCardLoader.java
+++ server/src/server/XmlCardLoader.java
@@ -8,10 +8,18 @@ import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import cards.Card;
+import cards.CardType;
+import cards.Class;
+import cards.Consumable;
+import cards.Curse;
+import cards.EquipSlot;
+import cards.Equipment;
 import cards.Monster;
+import cards.Race;
 import javafx.util.Pair;
 
 /*
@@ -47,13 +55,31 @@ public class XmlCardLoader {
 	private static final String rootTag = "resources";
 	private static final String cardTag = "card";
 	private static final String nameTag = "name";
-	private static final String typeTag = "type";
-	private static final String categoryTag = "category";
+	private static final String typeTag="type";
+	
+	
+	private static final String monsterTag = "monster";
+	private static final String monsterLevelTag="level";
+	
+	private static final String curseTag = "curse";
+	private static final String curseimmTag ="immediate";
+	
+	private static final String consumableTag = "consumable";
+	private static final String classTag = "class";
+	
+	private static final String raceTag = "race";
+	
+	private static final String equipmentTag = "equipment";
+	private static final String equipmentSlotTag="slot";
+	private static final String equipmentValueTag="value";
+	private static final String equipmentBonusTag="bonus";
+
 
 	// Tocca inserire
 
 	private ArrayList<Pair<String, Card>> loadedCards = null;
 	private Element root;
+	
 
 	public XmlCardLoader(File file) throws Exception {
 		// Use a document builder in order to get a document
@@ -83,54 +109,92 @@ public class XmlCardLoader {
 		for (int i = 0; i < cardList.getLength(); i++) {
 			Element cardElem = (Element) cardList.item(i);
 
-			// Retrieve name and path elements
+			// Retrieve name and type elements
 			Element cardNameElem = (Element) cardElem.getElementsByTagName(nameTag).item(0);
 			Element cardTypeElem = (Element) cardElem.getElementsByTagName(typeTag).item(0);
-			Element cardCategoryElem = (Element) cardElem.getElementsByTagName(categoryTag).item(0);
+			
+			//Element cardCategoryElem = (Element) cardElem.getElementsByTagName(categoryTag).item(0);
+
+			Element cardMonsterElem = (Element) cardElem.getElementsByTagName(monsterTag).item(0);
+			Element cardCurseElem = (Element) cardElem.getElementsByTagName(curseTag).item(0);
+			Element cardConsumableElem = (Element) cardElem.getElementsByTagName(consumableTag).item(0);
+			Element cardClassElem = (Element) cardElem.getElementsByTagName(classTag).item(0);
+			Element cardRaceElem = (Element) cardElem.getElementsByTagName(raceTag).item(0);
+			Element cardEquipmentElem = (Element) cardElem.getElementsByTagName(equipmentTag).item(0);
 
 			// Continue if null
-			if (cardNameElem == null || cardTypeElem == null || cardCategoryElem == null)
+			if (cardNameElem == null || cardTypeElem == null)
 				continue;
 
-			// Store name
+			// Store name and type
 			String name = cardNameElem.getTextContent().trim();
 			String type = cardTypeElem.getTextContent().trim();
-			String category = cardCategoryElem.getTextContent().trim();
+			
 
-			Card loadedCard = null;
+			
 
 			if (type == "Door") {
-				switch (category) {
+				if(cardMonsterElem!=null) {
+					
+						Element cardMonsterLevelElem = (Element) cardElem.getElementsByTagName(monsterLevelTag).item(0);
+						int level = Integer.parseInt(cardMonsterLevelElem.getTextContent().trim());
+						Monster loadedCard = new Monster(name, level);
+						loadedCards.add(new Pair<String, Card>(name, loadedCard));
+					}
+					else if(cardCurseElem!=null)
+						{
+						Element cardCurseImmElem = (Element) cardElem.getElementsByTagName(curseimmTag).item(0);
+						boolean immediate = Boolean.parseBoolean((cardCurseImmElem.getTextContent().trim()));
+						Curse loadedCard = new Curse(name,immediate);
+						loadedCards.add(new Pair<String, Card>(name, loadedCard));
+						} 
+					else if(cardConsumableElem!=null)
+					{
+						Consumable loadedCard = new Consumable(name, CardType.Door);
+						loadedCards.add(new Pair<String, Card>(name, loadedCard));
+					}
+					
+				
 
-				case "Monster":
-					loadedCard = new Monster(name, level);
-					break;
-				case "Curse":
-					break;
-				case "Consumable":
-					break;
+			}else if(type == "Treasure") {
+				
+				if(cardClassElem!=null)
+				{
+					Class loadedCard = new Class(name);
+					loadedCards.add(new Pair<String, Card>(name, loadedCard));
 				}
-
-			} else if (type == "Treasure") {
-				switch (category) {
-				case "Class":
-					break;
-				case "Race":
-					break;
-				case "Consumable":
-					break;
-				case "Equipment":
-					break;
-
+				else if(cardRaceElem!=null)
+				{
+					Race loadedCard = new Race(name);
+					loadedCards.add(new Pair<String, Card>(name, loadedCard));
+				} 
+				else if(cardConsumableElem!=null)
+				{
+					Consumable loadedCard = new Consumable(name, CardType.Treasure);
+					loadedCards.add(new Pair<String, Card>(name, loadedCard));
 				}
+				else if (cardEquipmentElem!=null)
+				{
+					Element cardEquipmentSlotlElem = (Element) cardElem.getElementsByTagName(equipmentSlotTag).item(0);
+					Element cardEquipmentValueElem = (Element) cardElem.getElementsByTagName(equipmentValueTag).item(0);
+					Element cardEquipmentBonusElem = (Element) cardElem.getElementsByTagName(equipmentBonusTag).item(0);
+					
+					Equipment loadedCard =new Equipment(name);
+					EquipSlot slot =loadedCard.getSlot(cardEquipmentSlotlElem.getTextContent().trim());
+					int value = Integer.parseInt(cardEquipmentValueElem.getTextContent().trim());
+					int bonus = Integer.parseInt(cardEquipmentBonusElem.getTextContent().trim());
+					loadedCard.createEquipment(slot, bonus, value);
+					loadedCards.add(new Pair<String, Card>(name, loadedCard));
+				}
+			
+
+				
 			} else
 				continue;
 
-			if (loadedCard != null)
-				loadedCards.add(new Pair<String, Card>(name, loadedCard));
 		}
 	}
-
+//problemone :D
 	public ArrayList<Pair<String, Card>> getCards() {
 		return loadedCards;
 	}
