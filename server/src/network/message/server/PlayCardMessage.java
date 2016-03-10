07fa7920790586f6308d033b28bf853e61be1a9b
Federico Simonetti <federicosimonetti94@gmail.com>
2016-03-10 19:22:06 +0100

    Modify playcardmessage

--- server/src/network/message/server/PlayCardMessage.java
+++ server/src/network/message/server/PlayCardMessage.java
@@ -1,17 +1,21 @@
  package network.message.server;
 
+import cards.CardType;
 import network.message.Message;
 
 /* Message used from server to tell a client to show or draw a card */
 
 public class PlayCardMessage extends Message{
 	private String cardName;
+	private CardType type;
 	private Action action;
 	public static enum Action {SHOW, DRAW};	//If SHOW just paint the card on the main table, if DRAW put that card in client's hand
+
 	
-	public PlayCardMessage(String cardName, Action action) {
+	public PlayCardMessage(String cardName,CardType type, Action action) {
 		super(Message.PLAY_CARD);
 		this.cardName = cardName;
+		this.type=type;
 		this.action = action;
 	}
 	
@@ -20,6 +24,9 @@ public class PlayCardMessage extends Message{
 	}
 	public Action getShowed(){
 		return action;
+	}
+	public CardType getType(){
+		return type;
 	}
 /*
 	@Override
