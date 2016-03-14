5ff68314e87b5d2a6a6e107ea12993b4165fced0
Enrico Gambini <enrico1036@gmail.com>
2016-03-14 01:14:36 +0100

    Resolved issue "I want to wait for a message coming from all the players" in Combat

--- server/src/game/Combat.java
+++ server/src/game/Combat.java
@@ -22,6 +22,7 @@ public class Combat extends StateMachine {
 		this.card = card;
 		playerWon = false;
 		promisedTreasure = 0;
+		helperPlayer = null;
 		playerBonus = 0;
 		states = new String[2];
 		states[0] = "Begin";
@@ -37,18 +38,23 @@ public class Combat extends StateMachine {
 	private void askForHelp() {
 		// start help request and communications between client and server
 		GameManager.getCurrentPlayer().sendMessage(new PopUpMessage("Do you want to ask for help? If yes, how many treasures do you want to gift? (N)", "No", "Yes", 0, card.getEarningTreasures(), 15000));
-		//waitfor popup answer (and message to return values)
+		// waitfor popup answer (and message to return values)
 		PopUpResultMessage ret = (PopUpResultMessage) GameManager.getInQueue().waitForMessage(GameManager.getCurrentPlayer().getUsername(), Message.CLT_POPUP_RESULT).getValue();
-		//if yes, set promisedTreasure and:
-		if(ret.isButton2Pressed()){
+		// if yes, set promisedTreasure and:
+		if (ret.isButton2Pressed()) {
 			for (Player player : GameManager.getPlayers()) { // ask to all except for the current player
 				if (player.getUsername() != GameManager.getCurrentPlayer().getUsername())
 					player.sendMessage(new PopUpMessage("Do you want to help " + GameManager.getCurrentPlayer().getUsername() + " for " + Integer.toString(promisedTreasure) + "? (N)", "No", "Yes", 10000));
 			}
-			
-			//TODO-ISSUE: I want to wait for a message coming from all the players
-			//ret = (PopUpResultMessage) GameManager.getInQueue().waitForMessage(???, Message.CLT_POPUP_RESULT).getValue();
-			// TODO: wait for popup answer, if yes manage it and set helperPlayer
+
+			for (int i = 0; i < GameManager.getPlayers().size() - 1; i++) {
+				// wait for popup answer, if yes set helperPlayer
+				ret = (PopUpResultMessage) GameManager.getInQueue().waitForMessage(Message.CLT_POPUP_RESULT).getValue();
+				if (ret.isButton2Pressed()) {
+					helperPlayer = GameManager.getPlayerByName(ret.getSender());
+					break;
+				}
+			}
 		}
 	}
 
@@ -62,30 +68,30 @@ public class Combat extends StateMachine {
 				// wait for popup answer
 				PopUpResultMessage popUpRet = (PopUpResultMessage) GameManager.getInQueue().waitForMessage(GameManager.getCurrentPlayer().getUsername(), Message.CLT_POPUP_RESULT).getValue();
 				// if yes
-				if(popUpRet.isButton2Pressed()){
+				if (popUpRet.isButton2Pressed()) {
 					allNo = false;
 					boolean cardSelected = false;
 					do {
-					SelectedCardMessage message = (SelectedCardMessage) GameManager.getInQueue().waitForMessage(player.getUsername(), Message.CLT_CARD_SELECTED).getValue();
-					// manage returned card (check if card.category is allowed)
-					Card card = player.getHandCard(message.getCardName());
-					switch(card.getCategory()) {
-					case Consumable:
-						cardSelected = true;
-						//TODO: card effect
-						break;
-					case Curse:
-						cardSelected = true;
-						//TODO: card effect
-						break;
-					default:
-						cardSelected = false;
-						break;
-					}
-					}while(!cardSelected);
+						SelectedCardMessage message = (SelectedCardMessage) GameManager.getInQueue().waitForMessage(player.getUsername(), Message.CLT_CARD_SELECTED).getValue();
+						// manage returned card (check if card.category is allowed)
+						Card card = player.getHandCard(message.getCardName());
+						switch (card.getCategory()) {
+						case Consumable:
+							cardSelected = true;
+							// TODO: card effect
+							break;
+						case Curse:
+							cardSelected = true;
+							// TODO: card effect
+							break;
+						default:
+							cardSelected = false;
+							break;
+						}
+					} while (!cardSelected);
 				}
 			}
-		}while(!allNo);
+		} while (!allNo);
 	}
 
 	private void end() {
