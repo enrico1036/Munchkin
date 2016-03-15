d440c61be93e1822d7cd80f31de1b9c7e92e17c2
Enrico Gambini <enrico1036@gmail.com>
2016-03-15 01:12:28 +0100

    Started implementing runEffect in Effect class

--- server/src/cards/Effect.java
+++ server/src/cards/Effect.java
@@ -3,14 +3,26 @@ package cards;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import com.sun.org.apache.xpath.internal.axes.WalkingIterator;
+
+import game.Decks;
+import game.GameManager;
+import network.message.server.PlayCardMessage;
+import network.message.server.PlayCardMessage.Action;
 import utils.StateMachine;
 
 public class Effect {
 	 
 	 public static void runEffect(StateMachine gamePhase, String effectID, HashMap<String, String> parameters) {
-//		 switch(){
-//		 call right method with gamePhase and needed parameters
-//		 }
+		 switch(effectID){
+		 //call right method with gamePhase and needed parameters
+		 case "loseLevel":
+			 loseLevel(Integer.parseInt((parameters.get("number_level"))));
+			 break;
+		 case "drawDoor":
+			 drawDoor(Integer.parseInt(parameters.get("number")), Boolean.getBoolean(parameters.get("show")));
+			 break;
+		 }
      }
 	 
 	 //???? non so se serve
@@ -30,11 +42,26 @@ public class Effect {
 	  
 	  //clone()
 	  
-	  //loseLevel(int number_level)
+	  private static void loseLevel(int number_level) {
+		  GameManager.getCurrentPlayer().leveleUp(-number_level);
+	  }
 	  
 	 
-	 //drawDoor(int number, boolean coperta)
-	 //drawTreasure(int number, boolean coperta)
+	 private static void drawDoor(int number, boolean show) {
+		 for (int i = 0; i < number; i++) {
+			 Card card = Decks.getDoorCard();
+			 GameManager.getCurrentPlayer().draw(card);
+			 if(show) {
+				 GameManager.broadcastMessage(new PlayCardMessage(card, Action.SHOW));
+				 try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			 }
+		}
+	 }
+	 //drawTreasure(int number, boolean show)
 	 
 	 //bonusEscape(int bonus)
 	 
