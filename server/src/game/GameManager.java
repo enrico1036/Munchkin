9198ddc074de61b2a8b12f0846ad1b21c738dd6a
Enrico Gambini <enrico1036@gmail.com>
2016-03-16 16:05:52 +0100

    Added initial draw of 2 door cards and 2 treasure cards

--- server/src/game/GameManager.java
+++ server/src/game/GameManager.java
@@ -14,88 +14,93 @@ import network.message.server.PopUpMessage;
 import network.message.server.StateUpdateMessage;
 import utils.Debug;
 
-public class GameManager{
+public class GameManager {
 	private static boolean playerWon;
 	private static MessageQueue queue = new MessageQueue();
 	private static ArrayList<game.Player> players = new ArrayList<>();
-	
-	
+
 	private static void begin() {
 		Debug.print("GameManager: Begin");
 		playerWon = false;
 		Decks.shuffleDecks();
+		for (Player player : players) {
+			for (int i = 0; i < 2; i++) {
+				player.draw(Decks.getDoorCard());
+				player.draw(Decks.getTreasureCard());
+			}
+		}
 		broadcastMessage(new StateUpdateMessage("begin"));
 	}
-	
+
 	private static void turn() {
 		Turn turn = new Turn();
-		while(turn.performStep());
+		while (turn.performStep())
+			;
 	}
-	
+
 	private static void end() {
 		Debug.print("GameManager: End");
 		Debug.print(players.get(0).getUsername() + "Won!!!");
 		broadcastMessage(new PopUpMessage(players.get(0).getUsername() + "Won!!!", 30000));
 	}
-	
+
 	private static void nextPlayer() {
 		players.add(players.get(0));
 		players.remove(0);
 	}
-	
+
 	public static void win() {
 		playerWon = true;
 	}
 
 	public static void startGame() {
 		begin();
-		while(!playerWon) {
+		while (!playerWon) {
 			nextPlayer();
 			turn();
 		}
-		end();		
+		end();
 	}
-	
-	
+
 	public static Player getCurrentPlayer() {
 		return players.get(0);
 	}
-	
-	public static final ArrayList<Player> getPlayers(){
+
+	public static final ArrayList<Player> getPlayers() {
 		return players;
 	}
-	
+
 	public static void broadcastMessage(Message message) {
 		for (Player player : GameManager.getPlayers()) {
 			player.sendMessage(message);
 		}
 	}
 
-	public static ArrayList<Player> readyPlayers(){
+	public static ArrayList<Player> readyPlayers() {
 		ArrayList<Player> readyP = new ArrayList<>();
-		
+
 		for (Player player : players) {
 			{
-				if(player.isLobby_ready())
-				readyP.add(player);
-					
+				if (player.isLobby_ready())
+					readyP.add(player);
+
 			}
 		}
 		return readyP;
 	}
-	
-	public static final MessageQueue getInQueue(){
+
+	public static final MessageQueue getInQueue() {
 		return queue;
 	}
-	
-	public static Player getPlayerByName(String playerName){
+
+	public static Player getPlayerByName(String playerName) {
 		Player selectedPlayer = null;
-		
+
 		for (Player player : players) {
 			{
-				if(player.getUsername() == playerName)
-				selectedPlayer = player;
-					
+				if (player.getUsername() == playerName)
+					selectedPlayer = player;
+
 			}
 		}
 		return selectedPlayer;
