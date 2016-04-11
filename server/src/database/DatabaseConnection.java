075ba809b74f1526031448f20d2c8d88ccfcfa75
Federico Simonetti <federicosimonetti94@gmail.com>
2016-04-12 01:38:22 +0200

    Create a class that communicates with an external db

---
+++ server/src/database/DatabaseConnection.java
@@ -0,0 +1,37 @@
+package database;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+
+		
+	  public static void main (String args[]){
+	    
+	    try {
+	      String driver = "com.mysql.jdbc.Driver";
+	      Class.forName(driver);
+	      String url = "jdbc:mysql://sql7.freesqldatabase.com:3306/sql7114844";
+	      String user = "sql7114844";
+	      String pass = "iTPjBncAf7";
+	      Connection con = DriverManager.getConnection(url, user, pass);
+	      Statement cmd = con.createStatement();
+	      String query = "select * from Prova ;";
+	      ResultSet res = cmd.executeQuery(query);
+	     while (res.next()) {
+	        System.out.print(res.getString("ID")+" ");
+	        System.out.println(res.getString("Nome"));
+	      }
+	     // res.close(); // chiudere le risorse DB è obbligatorio
+	      cmd.close();
+	      con.close();
+	    }
+
+	    catch (SQLException e){
+	      e.printStackTrace();
+	    }
+
+	    catch (ClassNotFoundException e){
+	      e.printStackTrace();
+	    }
+	  }
+	}
